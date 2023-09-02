9744c933c67a8922da58d5d1b41a17c4dac8df19
ajaypatil528 <124812032+ajaypatil528@users.noreply.github.com>
2023-09-01 23:27:03 -0400

    Update UnlockAccountForm.java

--- src/main/java/in/ajay/binding/UnlockAccountForm.java
+++ src/main/java/in/ajay/binding/UnlockAccountForm.java
@@ -2,4 +2,35 @@ package in.ajay.binding;
 
 public class UnlockAccountForm {
 
+	private String email;
+	private String tempPsswd;
+	private String newPsswd;
+	private String confirmPsswd;
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTempPsswd() {
+		return tempPsswd;
+	}
+	public void setTempPsswd(String tempPsswd) {
+		this.tempPsswd = tempPsswd;
+	}
+	public String getNewPsswd() {
+		return newPsswd;
+	}
+	public void setNewPsswd(String newPsswd) {
+		this.newPsswd = newPsswd;
+	}
+	public String getConfirmPsswd() {
+		return confirmPsswd;
+	}
+	public void setConfirmPsswd(String confirmPsswd) {
+		this.confirmPsswd = confirmPsswd;
+	}
+	
+	
 }
