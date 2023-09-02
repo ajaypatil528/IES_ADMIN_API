37309894179c32940db6b05b630b497726c2c131
ajaypatil528 <124812032+ajaypatil528@users.noreply.github.com>
2023-09-01 23:29:50 -0400

    Update AppConstants.java

--- src/main/java/in/ajay/constants/AppConstants.java
+++ src/main/java/in/ajay/constants/AppConstants.java
@@ -2,4 +2,27 @@ package in.ajay.constants;
 
 public class AppConstants {
 
+	public static final String FNAME = "${FNAME}";
+	
+	public static final String PWD = "${PWD}";
+	
+	public static final String EMAIL = "${EMAIL}";
+	
+	public static final String AP = "AP";
+	
+	public static final String DN = "DN";
+	
+	public static final String INVALID_CRED = "Invalid Credentials";
+	
+	public static final String UNLOCKED = "UNLOCKED";
+	
+	public static final String Y_STR = "Y";
+	
+	public static final String SUCCESS = "success";
+	
+	public static final String ACC_LOCKED = "Your Account Locked/In-Active";
+	
+	public static final String RECOVER_PWD = "Recover Pwd";
+	
+	public static final String FORGOT_PWD_BODY_FILE = "FORGOT_PWD_EMAIL_BODY.txt";
 }
