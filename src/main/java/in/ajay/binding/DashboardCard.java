b2b19835efeea318392a568f3411876c7ef37eab
ajaypatil528 <124812032+ajaypatil528@users.noreply.github.com>
2023-09-01 23:25:38 -0400

    Update DashboardCard.java

--- src/main/java/in/ajay/binding/DashboardCard.java
+++ src/main/java/in/ajay/binding/DashboardCard.java
@@ -2,4 +2,52 @@ package in.ajay.binding;
 
 public class DashboardCard {
 
+	private Long plansCnt;
+	private Long approvedCnt;
+	private Long deniedCnt;
+	private Double beniftAmtGiven;
+	private UserAccountForm user;
+
+	public Long getPlansCnt() {
+		return plansCnt;
+	}
+
+	public void setPlansCnt(Long plansCnt) {
+		this.plansCnt = plansCnt;
+	}
+
+	public Long getApprovedCnt() {
+		return approvedCnt;
+	}
+
+	public void setApprovedCnt(Long approvedCnt) {
+		this.approvedCnt = approvedCnt;
+	}
+
+	public Long getDeniedCnt() {
+		return deniedCnt;
+	}
+
+	public void setDeniedCnt(Long deniedCnt) {
+		this.deniedCnt = deniedCnt;
+	}
+
+	public Double getBeniftAmtGiven() {
+		return beniftAmtGiven;
+	}
+
+	public void setBeniftAmtGiven(Double beniftAmtGiven) {
+		this.beniftAmtGiven = beniftAmtGiven;
+	}
+
+	public UserAccountForm getUser() {
+		return user;
+	}
+
+	public void setUser(UserAccountForm user) {
+		this.user = user;
+	}
+	
+	
+	
 }
