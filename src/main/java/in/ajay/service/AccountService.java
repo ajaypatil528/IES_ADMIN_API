5d56aa5d4b654b87247a589a6e1d454a3579154f
ajaypatil528 <124812032+ajaypatil528@users.noreply.github.com>
2023-09-01 23:38:45 -0400

    Update AccountService.java

--- src/main/java/in/ajay/service/AccountService.java
+++ src/main/java/in/ajay/service/AccountService.java
@@ -1,5 +1,22 @@
 package in.ajay.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import in.ajay.binding.UnlockAccountForm;
+import in.ajay.binding.UserAccountForm;
+
+@Service
 public interface AccountService {
 
+	public boolean createUserAccount(UserAccountForm accForm);
+	
+	public List<UserAccountForm> fetchUserAccounts();
+	
+	public UserAccountForm getUserAccById(Integer accId);
+	
+	public String changeAccStatus(Integer accId, String status);
+	
+	public String unlockUserAccount(UnlockAccountForm unlockAccForm);
 }
