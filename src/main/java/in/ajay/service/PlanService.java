c9f053b54485c609d398237965ed6d18bea7daf5
ajaypatil528 <124812032+ajaypatil528@users.noreply.github.com>
2023-09-01 23:39:30 -0400

    Update PlanService.java

--- src/main/java/in/ajay/service/PlanService.java
+++ src/main/java/in/ajay/service/PlanService.java
@@ -1,5 +1,19 @@
 package in.ajay.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import in.ajay.binding.PlanForm;
+
+@Service
 public interface PlanService {
 
+	public boolean createPlan(PlanForm planForm);
+	
+	public List<PlanForm> fetchPlans();
+	
+	public PlanForm getPlanById(Integer planId);
+	
+	public String changePlanStatus(Integer planId, String status);
 }
