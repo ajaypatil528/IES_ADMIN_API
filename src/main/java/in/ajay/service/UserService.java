61c45a32c8011c4d58e5486149564bd8f7c2d494
ajaypatil528 <124812032+ajaypatil528@users.noreply.github.com>
2023-09-01 23:40:19 -0400

    Update UserService.java

--- src/main/java/in/ajay/service/UserService.java
+++ src/main/java/in/ajay/service/UserService.java
@@ -1,5 +1,20 @@
 package in.ajay.service;
 
+
+import org.springframework.stereotype.Service;
+
+import in.ajay.binding.DashboardCard;
+import in.ajay.binding.LoginForm;
+import in.ajay.binding.UserAccountForm;
+
+@Service 
 public interface UserService {
 
+	public String loginForm(LoginForm loginForm);
+	
+	public boolean recoverPassword(String email);
+	
+	public DashboardCard fetchDashboardInfo();
+	
+	public UserAccountForm getUserByEmail(String email);
 }
