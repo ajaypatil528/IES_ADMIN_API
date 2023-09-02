e7335e40d2f78f51514bc343054678bbd5b8f822
ajaypatil528 <124812032+ajaypatil528@users.noreply.github.com>
2023-09-01 23:37:54 -0400

    Update UserRestController.java

--- src/main/java/in/ajay/rest/UserRestController.java
+++ src/main/java/in/ajay/rest/UserRestController.java
@@ -1,5 +1,47 @@
 package in.ajay.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import in.ajay.binding.DashboardCard;
+import in.ajay.binding.LoginForm;
+import in.ajay.binding.UserAccountForm;
+import in.ajay.service.AccountService;
+import in.ajay.service.UserService;
+
+@RestController
 public class UserRestController {
 
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private AccountService accService;
+	
+	@PostMapping("/login")
+	public String login(@RequestBody LoginForm loginForm) {
+		
+		String status = userService.loginForm(loginForm);
+		
+		if(status.equals("success")) {
+			return "redirect:/dashboard?email="+loginForm.getEmail();
+		}else{
+			return status;
+		}
+	}
+	
+	@GetMapping("/dashboard")
+	public ResponseEntity<DashboardCard> buildDashboard(@RequestParam("email") String email){
+		UserAccountForm user = userService.getUserByEmail(email);
+		DashboardCard dashboardCard = userService.fetchDashboardInfo();
+		dashboardCard.setUser(user);
+		return new ResponseEntity<DashboardCard>(dashboardCard, HttpStatus.OK);
+	}
+	
 }
