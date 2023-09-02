887b24bb85fdd206e28ae67645585811b6b8baaf
ajaypatil528 <124812032+ajaypatil528@users.noreply.github.com>
2023-09-01 23:26:05 -0400

    Update LoginForm.java

--- src/main/java/in/ajay/binding/LoginForm.java
+++ src/main/java/in/ajay/binding/LoginForm.java
@@ -2,4 +2,20 @@ package in.ajay.binding;
 
 public class LoginForm {
 
+	private String email;
+	private String passwd;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPasswd() {
+		return passwd;
+	}
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+	
+	
 }
